c6071a349e4b4342dc1bef2ce6bd1eb191e1e4e8
Ceyf <louangrand@gmail.com>
2023-08-22 10:48:45 +0200

    premier draft des structures

--- struct_header.h
+++ struct_header.h
@@ -2,5 +2,24 @@
 #define STRUCT_HEADER_H
 //STRUCTURES
 
+typedef struct Batiment
+{
+    int niveau;
+    int cout_or;
+    int cout_bois;
+    int cout_mat_noire;
+    int villageois_necessaires;
+}Batiment;
+
+typedef struct Ressources_Joueur
+{
+    int nb_villageois;
+    int villageois_disponibles;
+    Batiment *batiments_construits;
+    int or_joueur;
+    int bois;
+    int mat_noire;
+    
+}Ressources_Joueur;
 
 #endif
