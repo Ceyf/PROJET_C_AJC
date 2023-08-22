14157a56b55ef9cb32523684a7a0d129b0109807
Gaetan <gaetan.fouillet@gmail.com>
2023-08-22 11:03:29 +0200

    add initialisation_joueur

--- fonctions.c
+++ fonctions.c
@@ -1,1 +1,11 @@
 #include "main_header.h"
+
+
+void initialisation_joueur(Ressources_Joueur *rjoueur)
+{
+    rjoueur->nb_villageois = 10;
+    rjoueur->villageois_disponibles = 10;
+    rjoueur->or_joueur = 1000;
+    rjoueur->bois = 200;
+    rjoueur->mat_noire = 100;
+}
